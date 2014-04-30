c1ad7fdefb6a23ba24411b583e4f2bade6bc4ce0
Predrag Gruevski <predrag.gruevski@gmail.com>
2014-04-29 20:04:29 -0400

    Partial implementation of randomized testing.

--- src/diego/examples/kvstore/common_test.go
+++ src/diego/examples/kvstore/common_test.go
@@ -1,10 +1,76 @@
 package kvstore
 
 import "testing"
+import "strconv"
+import "math/rand"
 import "diego/resolver"
+import "diego/tests"
+
+const trailingDistance = 50
+const randomKeyspaceSize = 50
 
 func setup()(*resolver.Resolver, *kvStore) {
-  return resolver.CreateResolver(makeState, 50), makeState().(*kvStore)
+  return makeResolver(), makeState().(*kvStore)
+}
+
+func makeResolver()*resolver.Resolver {
+  return resolver.CreateResolver(makeState, trailingDistance)
+}
+
+func generateRandomData(fns []func(int64, *rand.Rand)resolver.Transaction,
+                        correctId int64, rnd *rand.Rand) tests.TestDataItem {
+  choice := rnd.Intn(len(fns))
+  op := fns[choice](correctId, rnd)
+  return tests.MakeTestDataItem(op, tests.NoCheck, nil)
+}
+
+// TODO make this method not always return the correct id
+func generateTransactionId(correctId int64, rnd *rand.Rand) int64 {
+  return correctId
+}
+
+func generateKey(rnd *rand.Rand) string {
+  return strconv.Itoa(rnd.Intn(randomKeyspaceSize))
+}
+
+func generateValue(rnd *rand.Rand) string {
+  return generateKey(rnd)
+}
+
+func generateRandomLwwSet(correctId int64, rnd *rand.Rand)resolver.Transaction {
+  return &lwwSetOp{generateTransactionId(correctId, rnd),
+                   generateKey(rnd),
+                   generateValue(rnd)}
+}
+
+func generateRandomPessimisticSet(correctId int64, rnd *rand.Rand)resolver.Transaction {
+  return &pessimisticSetOp{generateTransactionId(correctId, rnd),
+                           generateKey(rnd),
+                           generateValue(rnd)}
+}
+
+func generateRandomAppend(correctId int64, rnd *rand.Rand)resolver.Transaction {
+  return &appendOp{generateTransactionId(correctId, rnd),
+                   generateKey(rnd),
+                   generateValue(rnd)}
+}
+
+func generateRandomFlipFlopAdd(correctId int64, rnd *rand.Rand)resolver.Transaction {
+  // TODO: fix this method
+  return &flipflopAddOp{0,
+                        generateKey(rnd),
+                        0,
+                        rnd.Intn(randomKeyspaceSize)}
+}
+
+func makeTestData(data []tests.TestDataItem, rnd *rand.Rand) {
+  fns := []func(int64, *rand.Rand)resolver.Transaction { generateRandomLwwSet,
+                                                         generateRandomPessimisticSet,
+                                                         generateRandomAppend,
+                                                         generateRandomFlipFlopAdd }
+  for i := 0; i < len(data); i++ {
+    data[i] = generateRandomData(fns, int64(i), rnd)
+  }
 }
 
 func stateEquals(a, b resolver.State)bool {
@@ -13,14 +79,17 @@ func stateEquals(a, b resolver.State)bool {
   return kv.Equals(kv2)
 }
 
-func keyValuePredicate(key string, value string) func(*testing.T, resolver.State) {
-  return func(t *testing.T, s resolver.State) {
+func keyValuePredicate(key string, value string) func(*testing.T, resolver.State)bool {
+  return func(t *testing.T, s resolver.State)bool {
     kv := s.(*kvStore)
     v, ok := kv.data[key]
     if !ok {
       t.Errorf("Expected key-value %s -> %s was missing", key, value)
+      return false
     } else if v != value {
       t.Errorf("Expected %s -> %s, got %s -> %s", key, value, key, v)
+      return false
     }
+    return true
   }
 }
