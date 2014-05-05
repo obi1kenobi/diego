f94d4b24ab80ab95d8ba4e6c796261400c92a9de
Kiril Vidimce <kiril@vidimce.org>
2014-05-05 15:16:04 -0400

    Insert predicate. Insert not there so test fails.

--- src/diego/examples/lego/common.go
+++ src/diego/examples/lego/common.go
@@ -27,3 +27,15 @@ func MakeVec3i(x, y, z int32) Vec3i {
 func MakeVec3f(x, y, z float32) Vec3f {
   return Vec3f { [3]float32 { x, y, z } }
 }
+
+func (vec *Vec2i) Equal(other Vec2i) bool {
+  return vec.data == other.data
+}
+
+func (vec *Vec3i) Equal(other Vec3i) bool {
+  return vec.data == other.data
+}
+
+func (vec *Vec3f) Equal(other Vec3f) bool {
+  return vec.data == other.data
+}
