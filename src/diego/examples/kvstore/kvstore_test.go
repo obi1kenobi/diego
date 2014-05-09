8fcd35cf00c1151781737c0bce84078c60184545
dxiao <dxiao@mit.edu>
2014-05-09 15:41:52 -0400

    Revert MakeRequestTokenGenerator to function factory

--- src/diego/examples/kvstore/kvstore_test.go
+++ src/diego/examples/kvstore/kvstore_test.go
@@ -7,10 +7,10 @@ func TestLwwSet(t *testing.T) {
   rs, s, nt := setup()
 
   testData := []tests.TestDataItem {
-    tests.MakeTestDataItem(&lwwSetOp{0, <-nt, "a", "b"}, tests.Success, keyValuePredicate("a", "b")),
-    tests.MakeTestDataItem(&lwwSetOp{0, <-nt, "a", "c"}, tests.Success, keyValuePredicate("a", "c")),
-    tests.MakeTestDataItem(&lwwSetOp{0, <-nt, "a", "abcd"}, tests.Success, keyValuePredicate("a", "abcd")),
-    tests.MakeTestDataItem(&lwwSetOp{3, <-nt, "a", "def"}, tests.Success, keyValuePredicate("a", "def")),
+    tests.MakeTestDataItem(&lwwSetOp{0, nt(), "a", "b"}, tests.Success, keyValuePredicate("a", "b")),
+    tests.MakeTestDataItem(&lwwSetOp{0, nt(), "a", "c"}, tests.Success, keyValuePredicate("a", "c")),
+    tests.MakeTestDataItem(&lwwSetOp{0, nt(), "a", "abcd"}, tests.Success, keyValuePredicate("a", "abcd")),
+    tests.MakeTestDataItem(&lwwSetOp{3, nt(), "a", "def"}, tests.Success, keyValuePredicate("a", "def")),
   }
 
   tests.RunSequentialTest(t, rs, testData, s, stateEquals)
@@ -20,10 +20,10 @@ func TestOptimisticSet(t *testing.T) {
   rs, s, nt := setup()
 
   testData := []tests.TestDataItem {
-    tests.MakeTestDataItem(&pessimisticSetOp{0, <-nt, "a", "b"}, tests.Success, keyValuePredicate("a", "b")),
-    tests.MakeTestDataItem(&pessimisticSetOp{0, <-nt, "a", "c"}, tests.Failure, keyValuePredicate("a", "b")),
-    tests.MakeTestDataItem(&pessimisticSetOp{1, <-nt, "a", "c"}, tests.Success, keyValuePredicate("a", "c")),
-    tests.MakeTestDataItem(&pessimisticSetOp{2, <-nt, "a", "abcd"}, tests.Success, keyValuePredicate("a", "abcd")),
+    tests.MakeTestDataItem(&pessimisticSetOp{0, nt(), "a", "b"}, tests.Success, keyValuePredicate("a", "b")),
+    tests.MakeTestDataItem(&pessimisticSetOp{0, nt(), "a", "c"}, tests.Failure, keyValuePredicate("a", "b")),
+    tests.MakeTestDataItem(&pessimisticSetOp{1, nt(), "a", "c"}, tests.Success, keyValuePredicate("a", "c")),
+    tests.MakeTestDataItem(&pessimisticSetOp{2, nt(), "a", "abcd"}, tests.Success, keyValuePredicate("a", "abcd")),
   }
 
   tests.RunSequentialTest(t, rs, testData, s, stateEquals)
@@ -33,16 +33,16 @@ func TestTestAndSet(t *testing.T) {
   rs, s, nt := setup()
 
   testData := []tests.TestDataItem {
-    tests.MakeTestDataItem(&testAndSetOp{0, <-nt, "a", "b"}, tests.Success, keyValuePredicate("a", "b")),
-    tests.MakeTestDataItem(&testAndSetOp{0, <-nt, "a", "c"}, tests.Failure, keyValuePredicate("a", "b")),
-    tests.MakeTestDataItem(&testAndSetOp{1, <-nt, "a", "c"}, tests.Success, keyValuePredicate("a", "c")),
-    tests.MakeTestDataItem(&lwwSetOp{0, <-nt, "a", "d"}, tests.Success, keyValuePredicate("a", "d")),
-    tests.MakeTestDataItem(&testAndSetOp{1, <-nt, "a", "c"}, tests.Failure, keyValuePredicate("a", "d")),
-    tests.MakeTestDataItem(&appendOp{0, <-nt, "a", "c"}, tests.Success, keyValuePredicate("a", "dc")),
-    tests.MakeTestDataItem(&appendOp{0, <-nt, "a", "e"}, tests.Success, keyValuePredicate("a", "dce")),
-    tests.MakeTestDataItem(&testAndSetOp{4, <-nt, "a", "e"}, tests.Failure, keyValuePredicate("a", "dce")),
-    tests.MakeTestDataItem(&testAndSetOp{5, <-nt, "a", "ef"}, tests.Success, keyValuePredicate("a", "ef")),
-    tests.MakeTestDataItem(&testAndSetOp{0, <-nt, "b", "ab"}, tests.Success, keyValuePredicate("b", "ab")),
+    tests.MakeTestDataItem(&testAndSetOp{0, nt(), "a", "b"}, tests.Success, keyValuePredicate("a", "b")),
+    tests.MakeTestDataItem(&testAndSetOp{0, nt(), "a", "c"}, tests.Failure, keyValuePredicate("a", "b")),
+    tests.MakeTestDataItem(&testAndSetOp{1, nt(), "a", "c"}, tests.Success, keyValuePredicate("a", "c")),
+    tests.MakeTestDataItem(&lwwSetOp{0, nt(), "a", "d"}, tests.Success, keyValuePredicate("a", "d")),
+    tests.MakeTestDataItem(&testAndSetOp{1, nt(), "a", "c"}, tests.Failure, keyValuePredicate("a", "d")),
+    tests.MakeTestDataItem(&appendOp{0, nt(), "a", "c"}, tests.Success, keyValuePredicate("a", "dc")),
+    tests.MakeTestDataItem(&appendOp{0, nt(), "a", "e"}, tests.Success, keyValuePredicate("a", "dce")),
+    tests.MakeTestDataItem(&testAndSetOp{4, nt(), "a", "e"}, tests.Failure, keyValuePredicate("a", "dce")),
+    tests.MakeTestDataItem(&testAndSetOp{5, nt(), "a", "ef"}, tests.Success, keyValuePredicate("a", "ef")),
+    tests.MakeTestDataItem(&testAndSetOp{0, nt(), "b", "ab"}, tests.Success, keyValuePredicate("b", "ab")),
   }
 
   tests.RunSequentialTest(t, rs, testData, s, stateEquals)
@@ -52,10 +52,10 @@ func TestAppend(t *testing.T) {
   rs, s, nt := setup()
 
   testData := []tests.TestDataItem {
-    tests.MakeTestDataItem(&appendOp{0, <-nt, "a", "b"}, tests.Success, keyValuePredicate("a", "b")),
-    tests.MakeTestDataItem(&appendOp{0, <-nt, "a", "c"}, tests.Success, keyValuePredicate("a", "bc")),
-    tests.MakeTestDataItem(&appendOp{1, <-nt, "a", "d"}, tests.Success, keyValuePredicate("a", "bcd")),
-    tests.MakeTestDataItem(&appendOp{3, <-nt, "a", "ef"}, tests.Success, keyValuePredicate("a", "bcdef")),
+    tests.MakeTestDataItem(&appendOp{0, nt(), "a", "b"}, tests.Success, keyValuePredicate("a", "b")),
+    tests.MakeTestDataItem(&appendOp{0, nt(), "a", "c"}, tests.Success, keyValuePredicate("a", "bc")),
+    tests.MakeTestDataItem(&appendOp{1, nt(), "a", "d"}, tests.Success, keyValuePredicate("a", "bcd")),
+    tests.MakeTestDataItem(&appendOp{3, nt(), "a", "ef"}, tests.Success, keyValuePredicate("a", "bcdef")),
   }
 
   tests.RunSequentialTest(t, rs, testData, s, stateEquals)
@@ -65,10 +65,10 @@ func TestFlipflopAdd(t *testing.T) {
   rs, s, nt := setup()
 
   testData := []tests.TestDataItem {
-    tests.MakeTestDataItem(&flipflopAddOp{0, <-nt, "a", 0, 2}, tests.Success, keyValuePredicate("a", "2")),
-    tests.MakeTestDataItem(&flipflopAddOp{1, <-nt, "a", 1, 1}, tests.Success, keyValuePredicate("a", "1")),
-    tests.MakeTestDataItem(&flipflopAddOp{1, <-nt, "a", 1, 3}, tests.Success, keyValuePredicate("a", "4")),
-    tests.MakeTestDataItem(&flipflopAddOp{0, <-nt, "a", 0, 4}, tests.Success, keyValuePredicate("a", "0")),
+    tests.MakeTestDataItem(&flipflopAddOp{0, nt(), "a", 0, 2}, tests.Success, keyValuePredicate("a", "2")),
+    tests.MakeTestDataItem(&flipflopAddOp{1, nt(), "a", 1, 1}, tests.Success, keyValuePredicate("a", "1")),
+    tests.MakeTestDataItem(&flipflopAddOp{1, nt(), "a", 1, 3}, tests.Success, keyValuePredicate("a", "4")),
+    tests.MakeTestDataItem(&flipflopAddOp{0, nt(), "a", 0, 4}, tests.Success, keyValuePredicate("a", "0")),
   }
 
   tests.RunSequentialTest(t, rs, testData, s, stateEquals)
